58ffeb8a3440f95a3c161fdcaa29a90f10825c23
Sajjad Toomari <toomaris@gmail.com>
2022-07-05 14:00:38 +0430

    Add project files.

---
+++ FileTransfer.Server/Services/UploaderService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+
+namespace FileTransfer.Server.Services
+{
+    public class UploaderService : Uploader.UploaderBase
+    {
+        public override async Task<UploaderReply> UploadFile(IAsyncStreamReader<UploaderRequest> requestStream, ServerCallContext context)
+        {
+            await foreach (var item in requestStream.ReadAllAsync())
+            {
+                
+            }
+            return null;
+        }
+    }
+}
